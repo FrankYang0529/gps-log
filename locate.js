7858f310552e40a14133d8ab419cdbe0129add77
howardchi <howardchi@fougood.com>
2014-05-21 20:19:49 +0800

    add view port

--- locate.js
+++ locate.js
@@ -26,8 +26,7 @@ function createMap (setplace) {
 	var map = L.map('map').setView(setplace, 13);
 
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-	    zoom: 100
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
 	L.marker(setplace).addTo(map)
